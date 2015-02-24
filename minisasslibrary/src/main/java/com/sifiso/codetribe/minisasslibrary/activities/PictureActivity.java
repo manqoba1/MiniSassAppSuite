69c1e0d4570b6bef46c82d7a5184e32d2c85f9e8
B-Chris <nkululekocgs@gmail.com>
2015-02-24 14:16:42 +0100

    Added PictureActivity  updated:  ResponseDTO &  ImagesDTO   SharedUtil   updates made for Picture Activity to work Added: ImageLocationClass ImageUtilClass PhotoCacheUtilClass PhotoUploadServiceClass

--- minisasslibrary/src/main/java/com/sifiso/codetribe/minisasslibrary/activities/PictureActivity.java
+++ minisasslibrary/src/main/java/com/sifiso/codetribe/minisasslibrary/activities/PictureActivity.java
@@ -34,7 +34,6 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
-import com.google.android.gms.internal.no;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
